f5fdcd6b412f8c30d7b589205d7739caf7bf76f4
mkshie <87657174+mkshie@users.noreply.github.com>
2025-02-22 02:43:54 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A,B;
+    while(1)
+    {
+        cin>> A >>B;
+        if(cin.eof()==true)
+            break;
+        cout<<A+B<<endl;
+    }
+        
+    return 0;
+}
+
+
