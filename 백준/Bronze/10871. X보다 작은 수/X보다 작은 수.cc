f6acefd2ad2a6fd398c44afe636283f27fefda35
mkshie <87657174+mkshie@users.noreply.github.com>
2025-02-22 02:45:20 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    
+    cin >> a >> b;
+    
+    int c[a];
+    
+    for(int i=0;i<a;i++)
+    {
+        cin >> c[i];
+       
+    }
+    
+    
+    
+    for(int k=0;k<a;k++)
+    {
+     if(c[k]>=b)
+     {
+      c[k]=0;   
+     }
+    }
+    
+    for(int k=0;k<a;k++)
+    {
+        if(c[k]!=0)
+            cout<<c[k]<<" ";
+    }
+    
+    
+    
+    
+    return 0;
+}
