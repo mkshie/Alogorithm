0cd301b1361f1814b310f542a4d976986b5875e5
mkshie <87657174+mkshie@users.noreply.github.com>
2025-02-22 02:46:11 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    int A[T];
+    int B[T];
+    for(int i=0;i<T;i++)
+    {
+        cin >>A[i] >>B[i];
+    }
+    
+    for(int k=0;k<T;k++)
+    {
+        cout<<A[k]+B[k]<<endl;
+    }
+        
+    return 0;
+}
