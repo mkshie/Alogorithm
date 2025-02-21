959838e8245d486af9e97990e30391a4b6b689f6
mkshie <87657174+mkshie@users.noreply.github.com>
2025-02-22 02:42:26 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k, q, l, b, n, p;
+	k = 1;
+	q = 1;
+	l = 2;
+	b = 2;
+	n = 2;
+	p = 8;
+
+	int k1, q1, l1, b1, n1, p1;
+	cin >> k1 >> q1 >> l1 >> b1 >> n1 >> p1;
+	cout << k - k1 << " " << q - q1 << " " << l - l1 << " " << b - b1 << " " << n - n1 << " " << p - p1;
+	return 0;
+}
