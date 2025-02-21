07bfec616f0186d0786e80fefce8e9da6c71e5a9
mkshie <87657174+mkshie@users.noreply.github.com>
2025-02-22 02:43:35 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 16 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+long long sum(vector<int> &a)
+{
+	long long ans=0;
+	for (size_t i = 0; i < a.size(); i++)
+	{
+		ans = ans + a[i];
+	}
+
+	return ans;
+
+}
