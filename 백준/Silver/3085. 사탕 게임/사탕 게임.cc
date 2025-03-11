f74cf75285ce0a31a2b2c4a2826198c25a54636c
mkshie <87657174+mkshie@users.noreply.github.com>
2025-03-12 01:35:09 +0900

    [Silver II] Title: 사탕 게임, Time: 24 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+char arr[51][51];
+int N;
+int max_cnt = 0, answer; // 최대값 , 정답 변수
+
+void row_check(){
+    int ch_num = 0;
+    for (int dy = 0; dy < N; dy++){
+        int count = 1;
+        for (int dx = 0; dx < N; dx++)
+        {
+           if(arr[dy][dx] == arr[dy][dx+1]){ // 다음 문자가 같으면 길이 증가
+               count++;
+           }
+           else{
+            if(max_cnt < count)
+                max_cnt = count;
+            count = 1;
+           }
+           
+        }
+    }
+}
+void column_check(){
+    int ch_num = 0;
+    for (int dx = 0; dx < N; dx++)
+    {
+        int count = 1;
+        for (int dy = 0; dy < N; dy++)
+        {
+            if (arr[dy][dx] == arr[dy+1][dx])
+            { // 다음 문자가 같으면 길이 증가
+                count++;
+            }
+            else
+            {
+                if (max_cnt < count)
+                    max_cnt = count;
+                count = 1;
+            }
+            
+        }
+    }
+}
+int main(){
+
+    cin.tie(NULL);
+
+    cin >> N;
+   
+    char color;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N;j++){
+            cin >> color;
+            arr[i][j] = color;
+        }
+    }
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N-1; j++){
+            swap(arr[i][j], arr[i][j + 1]); // 가로 부분 swap
+            row_check();
+            column_check();
+
+            swap(arr[i][j], arr[i][j + 1]); // 원래대로 복구
+
+            swap(arr[i][j], arr[i + 1][j]); // 세로 부분 swap
+
+            row_check();
+            column_check();
+
+            swap(arr[i][j], arr[i + 1][j]);
+        }
+    }
+    cout << max_cnt << "\n";
+}
