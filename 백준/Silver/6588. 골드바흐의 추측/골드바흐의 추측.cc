7e66f778fc7ea1fa35c754d48ece61bc464e6ce5
mkshie <87657174+mkshie@users.noreply.github.com>
2025-03-15 02:05:43 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 108 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -2,15 +2,19 @@
 
 using namespace std;
 
-//에라토스테네스의 체를 이용하여 미리 소수 판정 후
-//복잡도를 내리기 위해서 n 의 절반까지만 확인하기
+// 에라토스테네스의 체를 이용하여 미리 소수 판정 후
+// 복잡도를 내리기 위해서 n 의 절반까지만 확인하기
+// for 문에서  n/2 에 = 을 붙이지 않아서 3+3 = 6 이라는 케이스를 놓쳤음.
 
 bool arr[1000001] = {false};
 
 void check_odd(){
-    for (int i = 2; i < 500000;i++){
-        for (int j = 2; i * j < 1000001;j++){
-            arr[i * j] = true; // 소수를 에라토스테네스의 체로 미리 판정
+    for (int i = 2; i * i < 1000001; i++){
+        if(!arr[i]){
+            for (int j = 2; i * j < 1000001; j++)
+            {
+                arr[i * j] = true; // 소수를 에라토스테네스의 체로 미리 판정
+            }
         }
     }
 }
@@ -26,7 +30,7 @@ int main(){
         bool check_bool = false;
         if (n == 0)
             break;
-        for (int i = 3; i < n; i++)
+        for (int i = 3; i <= n / 2; i++)
         {
             if(arr[i]){ // 소수가 아니라면 패스
                 continue;
