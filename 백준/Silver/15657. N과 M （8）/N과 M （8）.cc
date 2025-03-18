3b37588fc7c32d8a9e5bf09c7d19a75f7e48214b
mkshie <87657174+mkshie@users.noreply.github.com>
2025-03-18 21:47:32 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<int> answer;
+
+void rec(int cnt,int size){ // 배열의 크기를 나타냄
+    if(size == M){
+        for(int a : answer){
+            cout << a << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = cnt; i < N;i++){
+        answer.push_back(arr[i]);
+        rec(i , size + 1);
+        answer.pop_back();
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N >> M;
+    for (int i = 0; i < N;i++){
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+    sort(arr.begin(), arr.end());
+    rec(0,0);
+}
