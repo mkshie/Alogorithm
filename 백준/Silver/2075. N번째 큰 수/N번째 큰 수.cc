439ffe7c4c13b44f740fdf0857d66575e6a8c740
mkshie <87657174+mkshie@users.noreply.github.com>
2025-05-18 18:30:31 +0900

    [Silver III] Title: N번째 큰 수, Time: 352 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N;
+    cin >> N;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 0; i < N;i++){
+        for (int j = 0; j < N;j++){
+            int num;
+            cin >> num;
+            pq.push(num);
+            if(pq.size() > N){
+                pq.pop();
+            }
+        }
+    }
+    // for (int i = 0; i < N-1;i++)
+    //     pq.pop();
+
+    cout << pq.top() << "\n";
+    return 0;
+}
