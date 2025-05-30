d7fe38ff5dd238b8014086f5f008647d3c0d40ad
mkshie <87657174+mkshie@users.noreply.github.com>
2025-05-30 23:08:27 +0900

    [Gold IV] Title: 운동, Time: 88 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 1e9;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int V, E;
+    cin >> V >> E;
+    vector<vector<int>> graph(V + 1, vector<int>(V + 1, INF));
+    for (int i = 1; i <= V;i++){
+        graph[i][i] = 0;
+    }
+    for (int i = 0; i < E;i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u][v] = w;
+    }
+
+    int answer = INF;
+
+    for (int k = 1; k <= V;k++){
+        for (int i = 1; i <= V;i++){
+            for (int j = 1; j <= V;j++) // 플로이드 워셜 알고리즘수행
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+        }
+    }
+
+    for (int i = 1; i <= V;i++){
+        for (int j = 1; j <= V;j++){
+            if(i==j) continue;
+            if(graph[i][j] != INF && graph[j][i] != INF){
+                answer = min(answer, graph[i][j] + graph[j][i]);
+            }
+        }
+    }
+    if(answer ==INF)
+        cout << "-1\n";
+    else{
+        cout << answer << "\n";
+    }
+    return 0;
+}
