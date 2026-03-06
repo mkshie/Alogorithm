67d96989c1eef924f2648ff1e9f7e6abc51d7449
mkshie <87657174+mkshie@users.noreply.github.com>
2026-03-07 05:01:49 +0900

    [Gold I] Title: 최솟값 찾기, Time: 1232 ms, Memory: 119044 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
+#include <deque>
 #include <vector>
 
 using namespace std;
 
+// 슬라이딩 윈도우 기법으로 문제를 풀어보자 우선순위큐는 출제자의 의도와 조금 다름
 int main()
 {
     ios::sync_with_stdio(false);
@@ -25,22 +27,32 @@ int main()
         arr.push_back(num);
     }
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
+    deque<pair<int, int>> dq;
     vector<int> answer;
 
     for (int i = 0; i < arr.size(); i++)
     {
         int left = i - L + 1, right = i;
 
-        pq.push({arr[i], i});
-
-        while (pq.top().second < left || pq.top().second > right)
+        if (!dq.empty())
         {
-            pq.pop();
+            while (!dq.empty() && dq.back().first > arr[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back({arr[i], i});
+        }
+        else
+        { // 비어있다면 일단 넣어주기
+            dq.push_back({arr[i], i});
         }
 
-        answer.push_back(pq.top().first);
+        if (dq.front().second < left || dq.front().second > right)
+        {
+            dq.pop_front();
+        }
+
+        answer.push_back(dq.front().first);
     }
 
     for (int num : answer)
@@ -51,3 +63,50 @@ int main()
 
     return 0;
 }
+
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int N, L;
+
+//     cin >> N >> L;
+
+//     vector<int> arr;
+
+//     for (int i = 0; i < N; i++)
+//     {
+//         int num;
+
+//         cin >> num;
+
+//         arr.push_back(num);
+//     }
+
+//     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+//     vector<int> answer;
+
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         int left = i - L + 1, right = i;
+
+//         pq.push({arr[i], i});
+
+//         while (pq.top().second < left || pq.top().second > right)
+//         {
+//             pq.pop();
+//         }
+
+//         answer.push_back(pq.top().first);
+//     }
+
+//     for (int num : answer)
+//     {
+//         cout << num << " ";
+//     }
+//     cout << "\n";
+
+//     return 0;
+// }
